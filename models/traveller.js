1a2677490f416226b991852cac59f665271e65d7
mack millar <mackmillar@gmail.com>
2021-01-07 16:36:41 +0000

    initial commit first test passed

---
+++ models/traveller.js
@@ -0,0 +1,33 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+    const result = this.journeys.map( (journey) => {
+        return journey.startLocation
+    });
+    return result
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
